4a3f79518131f48e91d80fb2e5a4993bd7b87e9b
cmlx <cmlx1218@163.com>
2019-06-19 17:39:29 +0800

    feat:初始化POJO

---
+++ src/main/java/com/fosterpet/cmlx/dto/UserDto.java
@@ -0,0 +1,20 @@
+package com.fosterpet.cmlx.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @Author cmlx
+ * @Date 2019-6-19 0019 16:55
+ * @Version 1.0
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDto {
+
+    private String userName;
+    private Integer age;
+
+}
